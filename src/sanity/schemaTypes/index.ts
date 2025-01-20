3133b56a921ebb8be91b796899d681d0f0fef604
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2025-01-21 00:20:38 +0500

    Backend Removed

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import projectSchema from '../schemas/projectSchema'
-import servicesSchema from '../schemas/servicesSchema'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [projectSchema, servicesSchema],
-}
