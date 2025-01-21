252ed780a50ec6ba82f2fff6294ebe3869bf286c
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2025-01-21 21:58:31 +0500

    Build Error resolved

---
+++ src/app/components/details/[slug]/static-params.ts
@@ -0,0 +1,11 @@
+// components/details/[slug]/static-params.ts
+
+export function generateStaticParams() {
+  return [
+    { slug: "Study.com" },
+    { slug: "ArtGenerator" },
+    { slug: "PROSPECTX" },
+    { slug: "IRTIQAAI" },
+    { slug: "Lazaza" },
+  ];
+}
---
+++ src/app/components/servicesDetails/[slug]/static-params.ts
@@ -0,0 +1,11 @@
+export function generateStaticParamsForServices() {
+    return [
+      { slug: "GenerativeAI" },
+      { slug: "AppDevelopment" },
+      { slug: "WebDevelopment" },
+      { slug: "DatabaseSecurity" },
+      { slug: "BlockchainDevelopment" },
+     { slug: "UIUXDesign" },
+     { slug: "GameDevelopment" },
+    ];
+  }
